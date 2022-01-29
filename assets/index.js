e520859bc0eef51c3cc01009eab6f74ee6f7a6fa
Blerand Ismaili <94268979+BlerandIsm@users.noreply.github.com>
2022-01-29 15:30:15 -0500

    final version

--- assets/index.js
+++ assets/index.js
@@ -3,12 +3,18 @@ $(document).ready(function(){
     var currentTime = moment().format("MMMM Do YYYY");
     var displayDate = document.getElementById("currentDay");
     displayDate.innerHTML = currentTime;
-    var curentHour = moment().format("HH");
+    var currentHour = moment().format("HH");
+
 
     //give button functionality to clear data from storage 
+    $("#clearFieldsBtn").click(function (event) {
+        event.preventDefault;
+        $("textarea").val("");
+        localStorage.clear();
+      });
 
 //time comparing for the user to be organized
-$(".time-div").each(function(){
+$(".time-div").each(function () {
     var timeDiv = $(this).attr("id").split("-")[1];
     
     if (currentHour == timeDiv) {
@@ -21,8 +27,7 @@ $(".time-div").each(function(){
       $(this).removeClass("future");
       $(this).addClass("past");
     }
-
-});
+  });
 
 
 //creating a local storage to save the values entered by user
@@ -31,7 +36,7 @@ $(".saveBtn").click(function (event) {
     var value = $(this).siblings(".time-block").val();
     var time = $(this).parent().attr("id").split("-")[1];
     localStorage.setItem(time, value);
-});
+  });
 
 //get items from locas storage and display in slot
 $("#hour-09 .time-block").val(localStorage.getItem("09"));
